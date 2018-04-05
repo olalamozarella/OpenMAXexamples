9414745211c4667b6cfd08c48715ad61bdbb1113
rpi3 <radovan.durec@gmail.com>
2018-04-05 18:57:21 +0200

    kozmeticka uprava

--- src/testcases/DecodeImage.h
+++ src/testcases/DecodeImage.h
@@ -12,8 +12,27 @@ public:
     DecodeImage();
     virtual ~DecodeImage();
 
+    /**
+     * @brief Init - initializes openmax core, components and opens/creates files
+     * @return true if successful, false otherwise
+     */
     bool Init( std::string inputFileName, std::string outputFileName );
+
+    /**
+     * @brief ReInit - closes currently open files and opens/creates files from parameter
+     * @note openmax core and components are not destroyed
+     * @return true if successful, false otherwise
+     */
+    bool ReInit( std::string inputFileName, std::string outputFileName );
+
+    /**
+     * @brief Run - contains main application logic
+     */
     void Run();
+
+    /**
+     * @brief Destroy - closes input/output files, deinitializes and destroys openmax components and core
+     */
     void Destroy();
 
 private:
